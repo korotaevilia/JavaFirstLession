f0450c56818f1d76be2a1adfe7dd1eb8893a420b
korotaevilia <ilia.korotaev@domru.ru>
2019-07-28 21:10:02 +0500

    мини правки

--- src/lession1/exercise4.java
+++ src/lession1/exercise4.java
@@ -3,10 +3,10 @@ package lession1;
 public class exercise4 {
     public static void main(String[] args) {
         System.out.println(equal(10,2));
-
     }
+
         public static boolean equal(int a,int b){
-            if ((a+b)>=10 &&(a+b)<=20) {return true;}
+            if ((a+b)>=10 &&(a+b)<=20) return true;
                 else return false;
                 }
 }
