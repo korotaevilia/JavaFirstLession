ffdf0f41d8e0a01c72ab16666edc093887c498d4
korotaevilia <ilia.korotaev@domru.ru>
2019-07-28 20:31:41 +0500

    5 упражнение с методом

---
+++ src/lession1/exercise5.java
@@ -0,0 +1,15 @@
+package lession1;
+
+public class exercise5 {
+    public static void main(String[] args) {
+        allocate(0);
+    }
+    //метод не возвращает значения а просто печатает результат при вызове
+        public static void allocate (int a){
+            if (a < 0) {
+                 System.out.println("Отрицательное число");}
+                else {
+                    System.out.println("Положительное число");}
+            }
+        }
+
