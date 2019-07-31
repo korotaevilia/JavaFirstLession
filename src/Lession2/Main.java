b1631bec23e9f94acf83496e93d622fcdddfe163
korotaevilia <ilia.korotaev@domru.ru>
2019-07-31 23:01:36 +0500

    4е задание

--- src/Lession2/Main.java
+++ src/Lession2/Main.java
@@ -1,6 +1,7 @@
 package Lession2;
 
 import java.sql.Array;
+import java.util.Arrays;
 
 public class Main {
 
@@ -8,6 +9,10 @@ public class Main {
         FirstExample(); //результат для 1го задания
         System.out.println();
         SecondExample();
+        System.out.println();
+        ThirdExample();
+        System.out.println();
+        FourthExample();
 
     }
 
@@ -36,5 +41,33 @@ public class Main {
 
     }
 
+    //Третье задание
+    public static void ThirdExample() {
+        int[] mass = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; (i < mass.length); i++) {
+            if (mass[i] < 6) {
+                mass[i] = mass[i] * 2;
+            }
+        }
+        System.out.print(Arrays.toString(mass));
+    }
 
+    //Четвертое задание
+    public static void FourthExample() {
+        int[][] mass = new int[4][4];
+        for (int i = 0; i < mass.length; i++) {
+            System.out.println();
+            for (int j = 0; j < mass[i].length; j++) {
+                if (i == j) {
+                    mass[i][j] = 1;
+                } else {
+                    mass[i][j] = 0;
+                }
+                System.out.print(mass[i][j]);
+
+            }
+
+        }
+    }
 }
+
