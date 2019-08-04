7ef0b2526c5f29a6823f5646be5275f5eb43e2a2
korotaevilia <ilia.korotaev@domru.ru>
2019-08-04 22:42:28 +0500

    коммент

--- src/Lession3/homework.java
+++ src/Lession3/homework.java
@@ -2,7 +2,7 @@ package Lession3;
 
 import java.util.Random;
 import java.util.Scanner;
-
+// 2 задание.
 public class homework {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
