e0eacba5685ae99921f6471e0763862f77b71def
korotaevilia <ilia.korotaev@domru.ru>
2019-08-18 20:52:23 +0500

    7й урок, дз из методички

--- src/Lession7/Plate.java
+++ src/Lession7/Plate.java
@@ -22,6 +22,10 @@ public class Plate {
     public void putFood(int n){
         food+=n;
     }
+
+    public int getFood() {
+        return food;
+    }
 }
 
 class Cat {
@@ -38,19 +42,37 @@ class Cat {
         happiness = p.decreaseFood(appetite);
     }
     public void CatInfo(){
-        System.out.println("Кот " + name + ", сыт? " + happiness + ", аппетит "+appetite);
+        System.out.println("Кот: " + name + ", сыт?: " + happiness + ", аппетит: "+appetite);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAppetite() {
+        return appetite;
+    }
+
+    public boolean isHappiness() {
+        return happiness;
     }
 }
 
 class MainClass {
     public static void main(String[] args) {
-        Cat cat = new Cat("Barsik", 5);
-        Plate plate = new Plate(100);
+        Cat cat1 = new Cat("Василий", 5);
+        Cat cat2 = new Cat("Мурзик",20);
+        Cat cat3 = new Cat("Пушок",26);
+        Cat[] arr = {cat1,cat2,cat3};
+        Plate plate = new Plate(50);
         plate.info();
-        cat.eat(plate);
-        plate.info();
-        cat.CatInfo();
-        plate.putFood(10);
-        plate.info();
+        for (int i =0 ; i< arr.length;i++){
+            System.out.println("Котик "+arr[i].getName()+ " кушает из тарелки c аппетитом " +arr[i].getAppetite());
+            arr[i].eat(plate);
+            System.out.println("Удалось покушать? Котик счастлив? : " + arr[i].isHappiness());
+            System.out.println("В тарелке осталось еды " + plate.getFood());
+            System.out.println();
+        }
+
     }
 }
